8a034cd251a5d503aca68034de7e40a2e7e6cd9c
zaoqi <zaomir@outlook.com>
2019-08-29 01:55:22 +0800

    以jest代替在此複雜環境不能正常工作的mocha

--- ecmascript3_commonjs/lang.test.js
+++ ecmascript3_commonjs/lang.test.js
@@ -1,15 +1,16 @@
-const assert = require('assert').strict
 const BiwaScheme = require('@zaoqi/biwascheme')
-const TheLanguage = require('../lang.js')
+const TheLanguage = require('./lang.js')
 BiwaScheme.define_libfunc('test-describe', 2, 2, (ar, intp)=>{
     BiwaScheme.assert_symbol(ar[0])
     BiwaScheme.assert_closure(ar[1])
     const name=ar[0].name
     const body=BiwaScheme.js_closure(ar[1], intp)
-    describe(name,()=>body())
+    test(name,()=>{
+        body()
+    })
 })
 BiwaScheme.define_libfunc('test-check-equal?', 2, 2, (ar)=>{
-    assert(BiwaScheme.equal(ar[0],ar[1]))
+    expect(BiwaScheme.equal(ar[0],ar[1])).toBe(true)
 })
 for(const [k, a, v] of [
     ['complex-parse', 1, TheLanguage.complex_parse],
